611bc597fd305cc9a2e230b6eaaede71eb4ae9b9
Frank-W-B <frank.burkholder@gmail.com>
2021-05-16 15:08:52 -0600

    Doesn't work

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ app = Flask(__name__)
 def index():
     return "Hello World."
 
+@app.route('/dashboard/')
+def show_dashboard():
+    plots = []
+    plots.append(make_plot())
+    return render_template('dashboard.html', plots=plots)
+
+def make_plot():
+    plot = figure(plot_height=300, sizing_mode='scale_width')
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [2**v for v in x]
+    plot.line(x, y, line_width=4)
+    script, div = components(plot)
+    return script, div
+
+
 if __name__ == '__main__':
     app.run(debug=True)
